339bed8e6354022e0fbf138a4b4e828763c83470
babaidass269030-art <babaidass269030@gmail.com>
2026-09-01 23:17:33 +0530

    Implement file chooser in MainActivity for image selection
    Added file chooser support in WebView for selecting multiple images.

--- android/app/src/main/java/com/fastimage/pdf/MainActivity.java
+++ android/app/src/main/java/com/fastimage/pdf/MainActivity.java
@@ -2,7 +2,11 @@ package com.fastimage.pdf;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -10,6 +14,8 @@ import android.webkit.WebViewClient;
 public class MainActivity extends Activity {
 
     private WebView webView;
+    private ValueCallback<Uri[]> uploadMessage;
+    private final static int FILECHOOSER_RESULTCODE = 1;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -29,9 +35,55 @@ public class MainActivity extends Activity {
             settings.setAllowUniversalAccessFromFileURLs(true);
 
             webView.setWebViewClient(new WebViewClient());
+
+            // গ্যালারি ও ফাইল পিকার ওপেন করার হ্যান্ডলার
+            webView.setWebChromeClient(new WebChromeClient() {
+                @Override
+                public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                    if (uploadMessage != null) {
+                        uploadMessage.onReceiveValue(null);
+                        uploadMessage = null;
+                    }
+
+                    uploadMessage = filePathCallback;
+
+                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    intent.setType("image/*");
+                    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+
+                    startActivityForResult(Intent.createChooser(intent, "Select Images"), FILECHOOSER_RESULTCODE);
+                    return true;
+                }
+            });
+
             webView.loadUrl("file:///android_asset/index.html");
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (uploadMessage == null) return;
+
+            Uri[] results = null;
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                if (data.getClipData() != null) {
+                    int count = data.getClipData().getItemCount();
+                    results = new Uri[count];
+                    for (int i = 0; i < count; i++) {
+                        results[i] = data.getClipData().getItemAt(i).getUri();
+                    }
+                } else if (data.getData() != null) {
+                    results = new Uri[]{data.getData()};
+                }
+            }
+            uploadMessage.onReceiveValue(results);
+            uploadMessage = null;
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
