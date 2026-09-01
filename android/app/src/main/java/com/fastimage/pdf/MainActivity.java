430f11afb4848177a353c8b8f6eec00a9d59b996
babaidass269030-art <babaidass269030@gmail.com>
2026-09-01 23:09:09 +0530

    Change MainActivity to extend Activity and add error handling
    Refactored MainActivity to extend Activity instead of AppCompatActivity and added error handling in onCreate method.

--- android/app/src/main/java/com/fastimage/pdf/MainActivity.java
+++ android/app/src/main/java/com/fastimage/pdf/MainActivity.java
@@ -1,13 +1,13 @@
 package com.fastimage.pdf;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private WebView webView;
 
@@ -16,20 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // XML ফাইলের ওপর নির্ভর না করে সরাসরি WebView তৈরি
-        webView = new WebView(this);
-        setContentView(webView);
+        try {
+            webView = new WebView(this);
+            setContentView(webView);
 
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
-        settings.setAllowFileAccessFromFileURLs(true);
-        settings.setAllowUniversalAccessFromFileURLs(true);
+            WebSettings settings = webView.getSettings();
+            settings.setJavaScriptEnabled(true);
+            settings.setDomStorageEnabled(true);
+            settings.setAllowFileAccess(true);
+            settings.setAllowContentAccess(true);
+            settings.setAllowFileAccessFromFileURLs(true);
+            settings.setAllowUniversalAccessFromFileURLs(true);
 
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("file:///android_asset/index.html");
+            webView.setWebViewClient(new WebViewClient());
+            webView.loadUrl("file:///android_asset/index.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
